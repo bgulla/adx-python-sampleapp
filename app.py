fc7f8a3c8137e393161585e0e07f940d49ab7fdd
brando <brandon@brandongulla.com>
2018-12-13 20:49:15 -0500

    fully functional

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash, g, abort, Blueprint
+import logging
+import sys
+import requests
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g, abort, Blueprint, jsonify
 from flask_restplus import Api, Resource, fields
 import zipcodes
+import json
 
 app = Flask(__name__)
 
@@ -30,24 +34,56 @@ class Todo(Resource):
         return zipcodes.matching(str(zipcode_id))
         #return zipcodes.matching(str(zipcode_id))
 
+def get_coords(zipcode, map_connection_string="http://10.0.5.11:8880"):
+    """
+    """
+    url = map_connection_string + "/api/1/zipcode/" + str(zipcode)
+    response = requests.get(url=url)
+    loc_map = response.json()[0]
+    return loc_map['lat'], loc_map['long'], response.text
+
 
 @app.route('/', methods=['GET', 'POST']) #this is called a decorator
 def home():
     map_url = "10.0.5.11:8002"
     map_style = "osm-bright"
+    lat = "36.67"
+    long = "-76.3"
+    loc_json = "empty"
 
     if request.method == 'POST':
-        if request.form['text']:
-            moo_text = request.form['text']
-            cow = "mooo!"
+        if request.form['zipcode']:
+            zip_code = request.form['zipcode']
+            lat, long, loc_json = get_coords(zip_code)
 #            application.logger.info('[Moo] '+ unicode(now.replace(microsecond=0)) + "\t" + request.remote_addr + "\t" + moo_text)
-    return render_template("index.html", cow=cow, map_url=map_url,map_style=map_style)
+    return render_template("index.html", map_url=map_url,map_style=map_style, lat=lat, long=long, loc_json = loc_json)
 
+@app.route('/dark', methods=['GET', 'POST']) #this is called a decorator
+def home2():
+    map_url = "10.0.5.11:8002"
+    map_style = "osm-bright"
+    lat = "36.67"
+    long = "-76.3"
+    loc_json = "empty"
+
+    if request.method == 'POST':
+        if request.form['zipcode']:
+            zip_code = request.form['zipcode']
+            lat, long, loc_json = get_coords(zip_code)
+#            application.logger.info('[Moo] '+ unicode(now.replace(microsecond=0)) + "\t" + request.remote_addr + "\t" + moo_text)
+    return render_template("dark.html", map_url=map_url,map_style=map_style, lat=lat, long=long, loc_json = loc_json)
+
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        # In production mode, add log handler to sys.stdout.
+        app.logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+        app.logger.setLevel(logging.INFO)
 
 if __name__ == '__main__':
+    app.logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+    app.logger.setLevel(logging.INFO)
     # TODO: Copy logger from the other example
-
-
     app.register_blueprint(api_v1)
     app.config['SWAGGER_UI_DOC_EXPANSION'] = "full"
     app.run(port=8880,debug=True, host="0.0.0.0")
