c9b8979dc998213b989548df0f206e4da03541f2
He who is brandon gulla <brandon@brandongulla.com>
2019-01-08 13:35:15 -0500

    breaking things

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,9 @@ import logging
 from logging.handlers import RotatingFileHandler
 from OpenSSL import SSL
 from sampleapp import app
+from mock import MagicMock
+
+mock = MagicMock()
 
 if __name__ == "__main__":
 #    context = SSL.Context(SSL.SSLv23_METHOD)
@@ -12,7 +15,9 @@ if __name__ == "__main__":
 #        cert_file = './python-tls.crt'
 #        key_file = './python-tls.key'
     #application.run(host="0.0.0.0",port=8443,ssl_context=(cert_file,key_file))
-    app.run(host="0.0.0.0",port=8080)
+#    with MagicMock.patch.object(getpass, "getuser", return_value='default'):
+    with mock.patch.object('getpass', "getuser", return_value='default'):
+        app.run(host="0.0.0.0",port=8080)
 
 
 
